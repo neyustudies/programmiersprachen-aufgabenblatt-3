256e29039cb7bf5a22bedc03329273bdf3b291da
neyustudies <science-cat@gmx.net>
2020-05-31 14:52:16 +0200

    task 3.3 front.test and back.test succeeded

--- source/tests.cpp
+++ source/tests.cpp
@@ -29,8 +29,8 @@ ListNode<T>* get_last_pointer(List<T> const& list_to_test) {return list_to_test.
 //test case definitions for 
 
 //test cases for element access of list
-//#include "sub_tests/front.test"
-//#include "sub_tests/back.test"
+#include "sub_tests/front.test"
+#include "sub_tests/back.test"
 
 //test cases for retrieving iterators
 //#include "sub_tests/begin.test"
