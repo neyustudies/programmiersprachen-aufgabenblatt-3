cf28ea10b26cbfe96c76574653401ab63348481f
neyustudies <science-cat@gmx.net>
2020-05-31 10:31:02 +0200

    comment out all include-statements in tests.cpp

--- source/tests.cpp
+++ source/tests.cpp
@@ -16,32 +16,32 @@ ListNode<T>* get_last_pointer(List<T> const& list_to_test) {return list_to_test.
 
 
 //test case definitions for default constructor of list as well as empty and size for empty list
-#include "sub_tests/default_constructed_list.test"
+//#include "sub_tests/default_constructed_list.test"
 
 //test case definitions for push methods of list
-#include "sub_tests/push_front.test"
-#include "sub_tests/push_back.test"
+//#include "sub_tests/push_front.test"
+//#include "sub_tests/push_back.test"
 
 //test case definitions for pop methods of list
-#include "sub_tests/pop_front.test"
-#include "sub_tests/pop_back.test"
+//#include "sub_tests/pop_front.test"
+//#include "sub_tests/pop_back.test"
 
 //test case definitions for 
 
 //test cases for element access of list
-#include "sub_tests/front.test"
-#include "sub_tests/back.test"
+//#include "sub_tests/front.test"
+//#include "sub_tests/back.test"
 
 //test cases for retrieving iterators
-#include "sub_tests/begin.test"
-#include "sub_tests/end.test"
+//#include "sub_tests/begin.test"
+//#include "sub_tests/end.test"
 
 //iterator tests
-#include "sub_tests/iterators/operator_star.test"
-#include "sub_tests/iterators/operator_arrow.test"
-#include "sub_tests/iterators/operator_equals.test"
-#include "sub_tests/iterators/operator_does_not_equal.test"
-#include "sub_tests/iterators/operator_iterate_forward.test"
+//#include "sub_tests/iterators/operator_star.test"
+//#include "sub_tests/iterators/operator_arrow.test"
+//#include "sub_tests/iterators/operator_equals.test"
+//#include "sub_tests/iterators/operator_does_not_equal.test"
+//#include "sub_tests/iterators/operator_iterate_forward.test"
 
 int main(int argc, char *argv[])
 {
